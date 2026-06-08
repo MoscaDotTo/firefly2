dedee8b6275a3def030d2a4d592b268f8804bdf7
Adam DeMuri <adam.demuri@gmail.com>
2026-06-07 18:02:48 -0600

    Run lint format

--- lib/device/Devices.hpp
+++ lib/device/Devices.hpp
@@ -83,7 +83,7 @@ const DeviceDescription will_backpack = DeviceDescription(
     RF_BOARD_MA_SUPPORTED, {
                                StripDescription(96, {}),
                                StripDescription(96, {Reversed}),
-    });
+                           });
 
 // Modify this variable to easily switch between devices.
 const DeviceDescription &current = scarf;
